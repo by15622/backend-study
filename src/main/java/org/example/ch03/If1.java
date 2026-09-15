b17bcf3573e4499ad63a5abc3ca21414cc164a60
seo hyeon ha <hyeonha1103@gmail.com>
2026-09-15 22:26:26 +0900

    feat: 0915 공부

---
+++ src/main/java/org/example/ch03/If1.java
@@ -0,0 +1,15 @@
+package org.example.ch03;
+
+public class If1 {
+
+  public static void main(String[] args) {
+    int age = 20; // 사용자 나이
+
+    if (age >= 18) {
+      System.out.println("성인입니다.");
+    }
+    if (age <= 18) {
+      System.out.println("미성년자입니다.");
+    }
+  }
+}
