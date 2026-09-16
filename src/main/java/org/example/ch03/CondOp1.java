ff2401a1070dec1efa05bcabf97f55be33eb7c87
seo hyeon ha <hyeonha1103@gmail.com>
2026-09-16 12:42:20 +0900

    feat: 0916 기초 공부

---
+++ src/main/java/org/example/ch03/CondOp1.java
@@ -0,0 +1,16 @@
+package org.example.ch03;
+
+public class CondOp1 {
+
+  public static void main(String[] args) {
+    int age = 18;
+    String status;
+    if (age >= 18) {
+      status = "성인";
+    } else {
+      status = "미성년자";
+    }
+    System.out.println("age = " + age + " status = " + status);
+  }
+
+}
---
+++ src/main/java/org/example/ch03/ex/CondOpEx.java
@@ -0,0 +1,13 @@
+package org.example.ch03.ex;
+
+public class CondOpEx {
+
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+
+    int max = (a > b) ? a : b;
+    System.out.println("더 큰 숫자는 " + max + "입니다.");
+  }
+
+}
